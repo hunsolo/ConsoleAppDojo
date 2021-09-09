3d4d05470e52dcd5bfa36bbbc5272bafe41278ba
Rendszergazda <Rendszergazda@WIN-QEEEOJUT28R>
2021-09-09 12:12:03 +0200

    Add project files.

---
+++ ConsoleAppDojo/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace ConsoleAppDojo
+{
+    using System;
+    using System.Buffers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new System.Buffers.ArrayBufferWriter<int>();
+            const int z = 16;
+            double y = 10 + 3;
+            var x = z * y;
+            Console.WriteLine("Hello: " + x.ToString());
+
+        }
+    }
+}
